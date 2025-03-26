b5ab4389f1b045a81a21b5909bf7a41814e7f387
Was_alone@vk.com <Was_alone@vk.com>
2025-03-26 21:26:23 +0500

    Init commit

---
+++ src/main/java/manakin/ru/stalcraftmonitor/StalcraftMonitorApplication.java
@@ -0,0 +1,13 @@
+package manakin.ru.stalcraftmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StalcraftMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StalcraftMonitorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/manakin/ru/stalcraftmonitor/StalcraftMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package manakin.ru.stalcraftmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StalcraftMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
